6920ff64ff71c3b0d7c22c0a3dc25ca3bad2340c
Haggar3037 <83113883+Haggar3037@users.noreply.github.com>
2022-07-08 22:06:03 +0700

    Готово

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
         <img src="img/logo.svg" alt="" class="header__container__logo">
         <nav>
             <ul class="header__container__menu">
-                <li class="header__container__menu__element"><a href="#">Это меню</a></li>
-                <li class="header__container__menu__element"><a href="#">Должно</a></li>
-                <li class="header__container__menu__element"><a href="#">Быть</a></li>
-                <li class="header__container__menu__element"><a href="#">Закреплённым</a></li>
-                <li class="header__container__menu__element"><a href="#">При скролле</a></li>
+                <li class="header__container__menu__element"><a href="3#">Это меню</a></li>
+                <li class="header__container__menu__element"><a href="3#">Должно</a></li>
+                <li class="header__container__menu__element"><a href="3#">Быть</a></li>
+                <li class="header__container__menu__element"><a href="3#">Закреплённым</a></li>
+                <li class="header__container__menu__element"><a href="3#">При скролле</a></li>
             </ul> 
             <button class="header__container__button"></button>
         </nav>
--- notFound.php
+++ notFound.php
@@ -1,10 +1,33 @@
-<link rel="stylesheet" href="css/notFound.css">
-<div class="not-found">
-    <img src="img/404.svg" alt="" class="not-found__img">
-    <h1 class="not-found__title">Страница не найдена</h1>
-    <p class="not-found__inform">
-    К сожалению, такой страницы нет. Либо администратор сайта ее удалил, либо ее никогда не существовало. 
-    Попробуйте проверить адрес страницы или начните с Главной.
-    </p>
-    <button class="not-found__button">На главную</button>
-</div>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/null.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/notFound.css">
+    <title>Тестовая работа</title>
+</head>
+<body>
+    <?php
+        include 'header.php';
+    ?>
+    <div class="not-found">
+        <img src="img/404.svg" alt="" class="not-found__img">
+        <h1 class="not-found__title">Страница не найдена</h1>
+        <p class="not-found__inform">
+        К сожалению, такой страницы нет. Либо администратор сайта ее удалил, либо ее никогда не существовало. 
+        Попробуйте проверить адрес страницы или начните с Главной.
+        </p>
+        <a class="not-found__button" href="/">На главную</a>
+    </div>
+    <?php
+        include 'footer.php';
+    ?>
+    <script src="/js/header.js"></script>
+</body>
+</html>
